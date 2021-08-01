45fd95c255c7c500a5cd95e551afb19457d349fc
M.Kostikov <admin@kosmuxa.ru>
2021-08-01 21:47:30 +0300

    Extend stack size of raw resources, belts and inserters

---
+++ data/resources.lua
@@ -0,0 +1,31 @@
+-------------------------------------------------------------------------
+-- Raw resources stack extend
+-------------------------------------------------------------------------  
+data.raw['item']['coal'].stack_size = 200;
+data.raw['item']['copper-ore'].stack_size = 200;
+data.raw['item']['iron-ore'].stack_size = 200;
+data.raw['item']['stone'].stack_size = 200;
+data.raw['item']['uranium-ore'].stack_size = 200;
+-------------------------------------------------------------------------
+-- Belts stack extend
+-------------------------------------------------------------------------
+data.raw['item']['transport-belt'].stack_size = 200;
+data.raw['item']['fast-transport-belt'].stack_size = 200;
+data.raw['item']['express-transport-belt'].stack_size = 200;
+
+data.raw['item']['underground-belt'].stack_size = 100;
+data.raw['item']['fast-underground-belt'].stack_size = 100;
+data.raw['item']['express-underground-belt'].stack_size = 100;
+
+data.raw['item']['splitter'].stack_size = 100;
+data.raw['item']['fast-splitter'].stack_size = 100;
+data.raw['item']['express-splitter'].stack_size = 100;
+-------------------------------------------------------------------------
+-- Inserters
+-------------------------------------------------------------------------
+data.raw['item']['inserter'].stack_size = 100;
+data.raw['item']['long-handed-inserter'].stack_size = 100;
+data.raw['item']['fast-inserter'].stack_size = 100;
+data.raw['item']['filter-inserter'].stack_size = 100;
+data.raw['item']['stack-inserter'].stack_size = 100;
+data.raw['item']['stack-filter-inserter'].stack_size = 100;
