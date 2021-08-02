940d7a8867b0efce891756fc7a91e4ebd030266b
M.Kostikov <admin@kosmuxa.ru>
2021-08-02 14:47:14 +0300

    Info icons scaling off

--- data/entities/medium-chest.lua
+++ data/entities/medium-chest.lua
@@ -23,7 +23,7 @@ data:extend({
 		},
 		fast_replaceable_group = "container",
 		inventory_size = 120,
-		scale_info_icons = true,
+		scale_info_icons = false,
 		open_sound = { filename = "__base__/sound/metallic-chest-open.ogg", volume=0.65 },
 		close_sound = { filename = "__base__/sound/metallic-chest-close.ogg", volume = 0.7 },
 		vehicle_impact_sound = sounds.generic_impact,
